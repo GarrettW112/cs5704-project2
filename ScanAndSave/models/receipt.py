950bc764507436c8270c5163026fd7efaa21f5bc
laurenspehl <131399540+laurenspehl@users.noreply.github.com>
2026-03-03 21:48:27 -0500

    sql file

--- ScanAndSave/models/receipt.py
+++ ScanAndSave/models/receipt.py
@@ -7,4 +7,11 @@ class Receipt(Base):
     receipt_id = Column(Integer, primary_key=True)
     user_id = Column(Integer, unique=True)
     store = Column(String(100), unique=True)
-    
+    # purchase_date DATE,
+    # subtotal DECIMAL(10,2),
+    # tax DECIMAL(10,2),
+    # total DECIMAL(10,2),
+    # created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    # FOREIGN KEY (user_id) 
+    #     REFERENCES users(id) 
+    #     ON DELETE CASCADE
