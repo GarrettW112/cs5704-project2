1f0107dfb6fe17c64e73c37c969d00a16d73ddd2
Garrett <garrettw02@vt.edu>
2026-05-01 15:04:54 -0400

    test

--- ScanAndSave/security.py
+++ ScanAndSave/security.py
@@ -17,3 +17,5 @@ def create_access_token(data: dict):
     expire = datetime.now(timezone.utc) + timedelta(minutes=30)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+#test
