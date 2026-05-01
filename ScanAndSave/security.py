5cfc460c7409d6cffd1d4952b2df1aef774311ec
Garrett <garrettw02@vt.edu>
2026-05-01 15:37:50 -0400

    Recipe Agent

--- ScanAndSave/security.py
+++ ScanAndSave/security.py
@@ -17,5 +17,3 @@ def create_access_token(data: dict):
     expire = datetime.now(timezone.utc) + timedelta(minutes=30)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-#test
